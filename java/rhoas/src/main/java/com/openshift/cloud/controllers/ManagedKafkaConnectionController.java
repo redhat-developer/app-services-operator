68bedb03ad0bb1d79e1c7b87d8e0d6f87cf9ac9e
Summers Pittman <supittma@redhat.com>
2021-01-18 14:53:06 -0500

    feat: base path and token are configurable

--- java/rhoas/src/main/java/com/openshift/cloud/controllers/ManagedKafkaConnectionController.java
+++ java/rhoas/src/main/java/com/openshift/cloud/controllers/ManagedKafkaConnectionController.java
@@ -25,6 +25,9 @@ import io.javaoperatorsdk.operator.api.DeleteControl;
 import io.javaoperatorsdk.operator.api.ResourceController;
 import io.javaoperatorsdk.operator.api.UpdateControl;
 import io.javaoperatorsdk.operator.processing.event.EventSourceManager;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import javax.annotation.PostConstruct;
 
 @Controller
 public class ManagedKafkaConnectionController implements ResourceController<ManagedKafkaConnection> {
@@ -33,17 +36,16 @@ public class ManagedKafkaConnectionController implements ResourceController<Mana
     private ApiClients k8sClients;
     private DefaultApi controlPanelApiClient;
 
+    @ConfigProperty(name = "client.basePath", defaultValue="https://api.stage.openshift.com")
+    String clientBasePath;
+
+    @ConfigProperty(name = "client.bearerToken")
+    String clientBearerToken;
+
+
     public ManagedKafkaConnectionController(ApiClients clients) {
         this.k8sClients = clients;
-        
-        ApiClient defaultClient = Configuration.getDefaultApiClient();
-        defaultClient.setBasePath("https://api.stage.openshift.com");
-        
-        // Configure HTTP bearer authorization: Bearer
-        HttpBearerAuth Bearer = (HttpBearerAuth) defaultClient.getAuthentication("Bearer");
-        Bearer.setBearerToken("eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICItNGVsY19WZE5fV3NPVVlmMkc0UXhyOEdjd0l4X0t0WFVDaXRhdExLbEx3In0.eyJleHAiOjE2MTA5ODM4MDIsImlhdCI6MTYxMDk4MjkwMiwiYXV0aF90aW1lIjoxNjEwOTgyODg3LCJqdGkiOiI2NTY1MTA2YS1mMTVmLTQ3ZWItOTdkNy0wMWMyOWFkYmJmOGUiLCJpc3MiOiJodHRwczovL3Nzby5yZWRoYXQuY29tL2F1dGgvcmVhbG1zL3JlZGhhdC1leHRlcm5hbCIsImF1ZCI6ImNsb3VkLXNlcnZpY2VzIiwic3ViIjoiZjo1MjhkNzZmZi1mNzA4LTQzZWQtOGNkNS1mZTE2ZjRmZTBjZTY6c3VwaXR0bWFfa2Fma2FfZGV2ZXhwIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoiY2xvdWQtc2VydmljZXMiLCJub25jZSI6ImZiNDRlNzhlLTk2MmEtNDRkMi04MjZkLWRhZDczNmM4MWJmYSIsInNlc3Npb25fc3RhdGUiOiJiOWVhNjdhZC1kN2ExLTQ3MGQtYTRjYi0xMmRlNzMxNjE0OGMiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbImh0dHBzOi8vcHJvZC5mb28ucmVkaGF0LmNvbToxMzM3IiwiaHR0cHM6Ly9hcGkuY2xvdWQucmVkaGF0LmNvbSIsImh0dHBzOi8vcWFwcm9kYXV0aC5jbG91ZC5yZWRoYXQuY29tIiwiaHR0cHM6Ly9jbG91ZC5vcGVuc2hpZnQuY29tIiwiaHR0cHM6Ly9wcm9kLmZvby5yZWRoYXQuY29tIiwiaHR0cHM6Ly9jbG91ZC5yZWRoYXQuY29tIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJhdXRoZW50aWNhdGVkIiwiY2FuZGxlcGluX3N5c3RlbV9hY2Nlc3Nfdmlld19lZGl0X3BlcnNvbmFsIl19LCJzY29wZSI6Im9wZW5pZCIsImFjY291bnRfbnVtYmVyIjoiNjk1NDYwOCIsImlzX2ludGVybmFsIjpmYWxzZSwiYWNjb3VudF9pZCI6IjUzODU4NTM1IiwiaXNfYWN0aXZlIjp0cnVlLCJvcmdfaWQiOiIxMzgzODYxNCIsImxhc3RfbmFtZSI6IlBpdG1hbiIsInR5cGUiOiJVc2VyIiwibG9jYWxlIjoiZW5fVVMiLCJmaXJzdF9uYW1lIjoiU3VtbWVycyIsImVtYWlsIjoic3VwaXR0bWFAcmVkaGF0LmNvbSIsInVzZXJuYW1lIjoic3VwaXR0bWFfa2Fma2FfZGV2ZXhwIiwiaXNfb3JnX2FkbWluIjpmYWxzZX0.u7y6zS-w17hUBDqtfsdVIRKMUk-C5u3eevcknnHxP6YK09WaAlF0MMYjVP-9S1DBCZVxhlfN-kKUf01Nqt0NFPnueOcUHkJpd8GyC8FYN7NGaW-PWp0su3H4KLvHruhy54cSChO59UHTq2oeX4toCUJelvz39DO95JI3a_OkGORfpLV_s9aV12_zLoNnJ_1pNL7VaZvH_uCiPeTk5p0YscY7gtybHtqleOBx7agWVRcnXu9Wg67qAD3_THzzQTrj_aprG0YCMWyePV3gZcV6aAQpmR4POdBUEK36opRITy2_QC-VLeYtAxtv5z0vLaqtvGtSB7yDV3hBGPGWxV6f4D7n2z8377McJUuk-Phnxu0LZW2xqt5B_JfdfBJ67KIQUYBe2SsL39JXTfA0ZM8XTvAaIRsYQXfXDt5o_pE7g4wIx8ddhftCsVSTvWtu0MjZObWj_wnjfU4PJtq1xyhncyjvcn5P8dCAcyk3OFqEYf6W79tz7uZzkjYJoEoqKJkDSc5yRe7guDWXU4l4Mi6MUtveFtULcnoHqkOMq-4RyPjB3Jq4iE770UQv_1pm_RvlBK9wQOh5V1U1eBIZS5Ui7BgNI__iVdmE6e91HWECJNFMihzNE7SiS8vaBIAs5akiI-nP0n2ZG1mSw1zifoS4BBx8ieiDiixfDkjRjzWfXc");
 
-        this.controlPanelApiClient = new DefaultApi(defaultClient);
 
     }
 
@@ -79,6 +81,25 @@ public class ManagedKafkaConnectionController implements ResourceController<Mana
     }
 
     /**
+     * client creation is handled after object construction because the
+     * client depends on injected values. This method is called by quarkus.
+     */
+    @PostConstruct
+    public void createClient() {
+
+        ApiClient defaultClient = Configuration.getDefaultApiClient();
+        defaultClient.setBasePath(clientBasePath);
+
+        // Configure HTTP bearer authorization: Bearer
+        HttpBearerAuth Bearer = (HttpBearerAuth) defaultClient.getAuthentication("Bearer");
+        Bearer.setBearerToken(clientBearerToken);
+
+        this.controlPanelApiClient = new DefaultApi(defaultClient);
+
+    }
+
+
+    /**
      * In init typically you might want to register event sources.
      *
      * @param eventSourceManager
@@ -86,6 +107,9 @@ public class ManagedKafkaConnectionController implements ResourceController<Mana
     @Override
     public void init(EventSourceManager eventSourceManager) {
         LOG.info("Init! This is where we would add watches for child resources");
+
+
+
     }
 
     private MixedOperation<ManagedKafkaConnection, ManagedKafkaConnectionList, Resource<ManagedKafkaConnection>> getKafkaUserClient(
