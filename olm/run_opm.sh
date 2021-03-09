e1165f56ea680eacee8594b1da7c46b5b58d87b8
Summers Pittman <secondsun@gmail.com>
2021-03-09 11:50:01 -0500

    build: parameterized build

--- olm/run_opm.sh
+++ olm/run_opm.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+
+
 has_image () {
-  podman manifest inspect quay.io/rhoas/service-operator-registry:autolatest > /dev/null; 
+  podman manifest inspect quay.io/$1/service-operator-registry:autolatest > /dev/null; 
 }
 
 if  has_image ; then 
-  opm index add --bundles quay.io/rhoas/service-operator-bundle:$VERSION  --mode=semver --generate -f quay.io/rhoas/service-operator-registry:autolatest
+  opm index add --bundles quay.io/$1/service-operator-bundle:$VERSION  --mode=semver --generate -f quay.io/$1/service-operator-registry:autolatest
 else 
-  opm index add --bundles quay.io/rhoas/service-operator-bundle:$VERSION  --mode=semver --generate
+  opm index add --bundles quay.io/$1/service-operator-bundle:$VERSION  --mode=semver --generate
 fi
