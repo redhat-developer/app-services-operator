14d3533febad862b47fdc622ecc0250471da81ac
Summers Pittman <secondsun@gmail.com>
2021-03-30 15:59:20 -0400

    test: testing that userKafkasUpdated

--- source/rhoas/src/main/java/com/openshift/cloud/beans/MockKafkaApiClient.java
+++ source/rhoas/src/main/java/com/openshift/cloud/beans/MockKafkaApiClient.java
@@ -7,12 +7,40 @@ import com.openshift.cloud.controllers.ConditionAwareException;
 import com.openshift.cloud.v1alpha.models.CloudServiceAccountRequest;
 import com.openshift.cloud.v1alpha.models.CloudServiceAccountRequestSpec;
 import java.time.OffsetDateTime;
+import java.util.HashMap;
+import java.util.Map;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Alternative;
 
 @ApplicationScoped
 @Alternative
 public class MockKafkaApiClient extends KafkaApiClient {
+
+  private static final KafkaRequest DEFAULT_KAFKA;
+
+  private Map<String, KafkaRequest> kafkaRequestsById = new HashMap<>();
+
+  static {
+    var request = new KafkaRequest();
+
+    request.bootstrapServerHost("testHost");
+    request.cloudProvider("cloudProvider");
+    request.createdAt(OffsetDateTime.now());
+    request.name("name");
+    request.owner("owner");
+    request.setId("1234567890");
+    request.status("status");
+
+    request.setUpdatedAt(OffsetDateTime.now());
+
+    request.setRegion("region");
+    DEFAULT_KAFKA = request;
+  }
+
+  public MockKafkaApiClient() {
+    kafkaRequestsById.put(DEFAULT_KAFKA.getId(), DEFAULT_KAFKA);
+  }
+
   @Override
   public void createSecretForServiceAccount(
       CloudServiceAccountRequest resource, ServiceAccount serviceAccount)
@@ -55,21 +83,20 @@ public class MockKafkaApiClient extends KafkaApiClient {
   @Override
   public KafkaRequestList listKafkas(String accessToken) throws ConditionAwareException {
     var list = new KafkaRequestList();
-    var request = new KafkaRequest();
-
-    request.bootstrapServerHost("testHost");
-    request.cloudProvider("cloudProvider");
-    request.createdAt(OffsetDateTime.now());
-    request.name("name");
-    request.owner("owner");
-    request.setId("1234567890");
-    request.status("status");
-
-    request.setUpdatedAt(OffsetDateTime.now());
-
-    request.setRegion("region");
-
-    list.addItemsItem(request);
+    for (KafkaRequest request : kafkaRequestsById.values()) {
+      list.addItemsItem(request);
+    }
     return list;
+  }
+
+  /** This method adds a kafka to this mock that it returns. By default we return a DEFAULT_KAFKA */
+  public void addKafka(KafkaRequest newKafka) {
+    kafkaRequestsById.put(newKafka.getId(), newKafka);
+  }
+
+  /** Resets the kafka map to have only DEFAULT_KAFKA */
+  public void resetKafkas() {
+    kafkaRequestsById.clear();
+    kafkaRequestsById.put(DEFAULT_KAFKA.getId(), DEFAULT_KAFKA);
   }
 }
