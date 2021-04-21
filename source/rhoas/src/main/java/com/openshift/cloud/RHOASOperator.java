4396215df61a1827936c2c00e6328dd9a2f01dd0
Wojciech Trocki <wtrocki@redhat.com>
2021-04-21 13:03:20 +0100

    fdsf

--- source/rhoas/src/main/java/com/openshift/cloud/RHOASOperator.java
+++ source/rhoas/src/main/java/com/openshift/cloud/RHOASOperator.java
@@ -10,6 +10,8 @@ import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
 import javax.inject.Inject;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
 
 @QuarkusMain
