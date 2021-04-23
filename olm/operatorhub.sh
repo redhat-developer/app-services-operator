332dfadb511b71c16098d8c4c0ba40a3bf68e0c2
Wojciech Trocki <wtrocki@redhat.com>
2021-04-23 14:46:52 +0100

    fix: improve script

--- olm/operatorhub.sh
+++ olm/operatorhub.sh
@@ -1,7 +1,13 @@
 ## Script for manual release to operator hub
+## After script finishes please manually 
 
 # git clone git@github.com:operator-framework/community-operators.git
-cp -Rf ./olm-catalog/rhoas-operator/`cat version` ./community-operators/community-operators/rhoas-operator
+VERSION=`cat version`
+mkdir ./community-operators/community-operators/rhoas-operator/${VERSION}
+cp -Rf ./olm-catalog/rhoas-operator/${VERSION}/manifests ./community-operators/community-operators/rhoas-operator/${VERSION}
+cp -Rf ./olm-catalog/rhoas-operator/${VERSION}/metadata ./community-operators/community-operators/rhoas-operator/${VERSION}
 
-cd ./community-operators && git add --all \
-    && git commit -a -m"RHOAS Operator version `cat version`"
+cd ./community-operators && \
+    git checkout -b rhoas-operator-${VERSION}  && \
+    git add --all && \
+    git commit --signoff -m"chore: update rhoas operator ${VERSION}"
