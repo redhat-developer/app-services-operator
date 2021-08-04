31fe3c59f545c6e1b273d3a32683a8a620fc54fe
Hoyt Summers Pittman <secondsun@gmail.com>
2021-08-04 10:34:33 -0400

    fix: using new operatorhub repository

--- olm/operatorhub.sh
+++ olm/operatorhub.sh
@@ -1,13 +1,13 @@
 ## Script for manual release to operator hub
 ## After script finishes please manually 
 
-# git clone git@github.com:operator-framework/community-operators.git
+ git clone git@github.com:redhat-openshift-ecosystem/community-operators-prod.git
 VERSION=`cat version`
-mkdir ./community-operators/community-operators/rhoas-operator/${VERSION}
-cp -Rf ./olm-catalog/rhoas-operator/${VERSION}/manifests ./community-operators/community-operators/rhoas-operator/${VERSION}
-cp -Rf ./olm-catalog/rhoas-operator/${VERSION}/metadata ./community-operators/community-operators/rhoas-operator/${VERSION}
+mkdir ./community-operators-prod/operators/rhoas-operator/${VERSION}
+cp -Rf ./olm-catalog/rhoas-operator/${VERSION}/manifests ./community-operators-prod/operators/rhoas-operator/${VERSION}
+cp -Rf ./olm-catalog/rhoas-operator/${VERSION}/metadata ./community-operators-prod/operators/rhoas-operator/${VERSION}
 
-cd ./community-operators && \
+cd ./community-operators-prod && \
     git checkout -b rhoas-operator-${VERSION}  && \
     git add --all && \
     git commit --signoff -m"chore: update rhoas operator ${VERSION}"
