124bd5e27a1777f240a652dd04afe96ae2f088fa
Summers Pittman <secondsun@gmail.com>
2021-08-23 16:49:55 -0400

    fix: apdating apis and fixing tests

---
+++ source/rhoas/src/main/java/com/openshift/cloud/beans/MockServiceRegistryApiClient.java
@@ -0,0 +1,30 @@
+package com.openshift.cloud.beans;
+
+import com.openshift.cloud.api.srs.invoker.ApiException;
+import com.openshift.cloud.api.srs.models.RegistryRest;
+import com.openshift.cloud.controllers.ConditionAwareException;
+import com.openshift.cloud.controllers.ConditionUtil;
+import com.openshift.cloud.v1alpha.models.KafkaCondition;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Alternative;
+import java.util.ArrayList;
+import java.util.List;
+
+@ApplicationScoped
+@Alternative
+public class MockServiceRegistryApiClient extends ServiceRegistryApiClient{
+
+    @ConfigProperty(name = "rhoas.client.apiBasePath")
+    String clientBasePath;
+
+    public List<RegistryRest> listRegistries(String accessToken) throws ConditionAwareException {
+            return new ArrayList<>();
+    }
+
+    public RegistryRest getServiceRegistryById(String registryId, String accessToken) throws ConditionAwareException {
+        return new RegistryRest();
+    }
+
+}
